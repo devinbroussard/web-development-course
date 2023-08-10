22d35cde495048196e2cbee30642a284e40e6b54
Devin Broussard <devin.ray.broussard@gmail.com>
2023-08-10 09:51:33 -0500

    Complete Node native module challenge

--- node/2.2 Native Modules/index.js
+++ node/2.2 Native Modules/index.js
@@ -1,6 +1,12 @@
 const fs = require("fs")
-fs.writeFile('./message.txt', 'Hello from Node!', (error) => {
+
+
+/* fs.writeFile('./message.txt', 'Hello from Node!', (error) => {
   if (error) throw error
   console.log('Save successful!')
-})
+}) */
 
+fs.readFile('./message.txt', 'utf8', (err, data) => {
+  if (err) throw err
+  console.log(data)
+} )
