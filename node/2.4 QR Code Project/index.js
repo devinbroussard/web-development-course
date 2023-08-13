aa3bc34192e3b14e295eb3cc1bee35862deb04b1
Devin Broussard <devin.ray.broussard@gmail.com>
2023-08-12 20:10:12 -0500

    Finish QR project

--- node/2.4 QR Code Project/index.js
+++ node/2.4 QR Code Project/index.js
@@ -4,5 +4,28 @@
 3. Create a txt file to save the user input using the native fs node module.
 */
 
+import fs from 'fs'
 import inquirer from 'inquirer'
 import qr from 'qr-image'
+
+const question = {
+  type: 'input',
+  name: 'url',
+  message: 'Enter a URL to generate a QR code for.'
+}
+
+inquirer
+  .prompt([
+    question
+  ])
+  .then((answers) => {
+    if (answers?.url === null || answers?.url === undefined) return
+    
+    var qrSvg = qr.image(answers.url, { type: 'svg' })
+    qrSvg.pipe(fs.createWriteStream('url.svg'))
+
+    fs.promises.writeFile('url.txt', answers.url)
+  })
+  .catch((error) => {
+    throw error
+  });
